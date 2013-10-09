44b1d393701b05a56c82ca9603accaa55940d4dd
tomdickin <thomasdickin@googlemail.com>
2013-10-09 23:54:01 +0100

    Adding test for mixed alphanum and non alphanum char string

--- tests.py
+++ tests.py
@@ -4,7 +4,11 @@ import replace
 
 class ReplaceTestCase(unittest.TestCase):
 
-    def test_alpha_numeric_is_unchanged(self):
+    def test_alpha_numeric(self):
         text = 'abc123'
-        output_text = replace.alpha_num('abc123')
+        output_text = replace.alpha_num(text)
         self.assertEqual(text, output_text)
+
+        text = 'abc 123 **'
+        output_text = replace.alpha_num(text)
+        self.assertNotEqual(text, output_text)
