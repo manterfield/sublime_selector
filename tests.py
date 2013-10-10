19e4937eb1fd259e1fb801cac9ce1b4d0c1a9ca0
tomdickin <thomasdickin@googlemail.com>
2013-10-10 20:15:40 +0100

    Increasing test coverage

--- tests.py
+++ tests.py
@@ -6,9 +6,51 @@ class ReplaceTestCase(unittest.TestCase):
 
     def test_alpha_numeric(self):
         text = 'abc123'
+        expected_text = 'abc123'
         output_text = replace.alpha_num(text)
-        self.assertEqual(text, output_text)
+        self.assertEqual(expected_text, output_text)
 
-        text = 'abc 123 **'
+        text = 'abc 123 ***'
+        expected_text = 'abc123'
         output_text = replace.alpha_num(text)
-        self.assertNotEqual(text, output_text)
+        self.assertEqual(expected_text, output_text)
+
+        text = '***'
+        expected_text = ''
+        output_text = replace.alpha_num(text)
+        self.assertEqual(expected_text, output_text)
+
+    def test_slugify(self):
+        text = 'foo-bar-buzz'
+        expected_text = 'foo-bar-buzz'
+        output_text = replace.slugify(text)
+        self.assertEqual(expected_text, output_text)
+
+        text = 'foo bar buzz !*('
+        expected_text = 'foo-bar-buzz'
+        output_text = replace.slugify(text)
+        self.assertEqual(expected_text, output_text)
+
+        text = ' '
+        expected_text = ''
+        output_text = replace.slugify(text)
+        self.assertEqual(expected_text, output_text)
+
+    def test_wrap(self):
+        text = 'foo'
+        expected_text = '<b>foo</b>'
+        output_text = replace.wrap(text, '<b>', '</b>')
+        self.assertEqual(expected_text, output_text)
+
+    def test_classify(self):
+        text = 'foo bar'
+        expected_text = 'FooBar'
+        output_text = replace.classify(text)
+        self.assertEqual(expected_text, output_text)
+
+    def test_title(self):
+        text = 'i love biscuits'
+        expected_text = 'I Love Biscuits'
+        output_text = replace.title(text)
+        self.assertEqual(expected_text, output_text)
+
